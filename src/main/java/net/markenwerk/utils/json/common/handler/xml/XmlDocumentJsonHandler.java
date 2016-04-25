88d389d0d6f2f983e371887c4b17d92fb46c8672
Torsten Krause <tk@markenwerk.net>
2016-04-25 18:41:15 +0200

    initial commit

---
+++ src/main/java/net/markenwerk/utils/json/common/handler/xml/XmlDocumentJsonHandler.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright (c) 2016 Torsten Krause, Markenwerk GmbH
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.markenwerk.utils.json.common.handler.xml;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import net.markenwerk.utils.json.common.InvalidJsonValueException;
+import net.markenwerk.utils.json.handler.IdleJsonHandler;
+import net.markenwerk.utils.json.handler.JsonHandler;
+import net.markenwerk.utils.json.handler.JsonHandlingException;
+
+/**
+ * A {@link XmlDocumentJsonHandler} is a {@link JsonHandler} that creates a XML
+ * {@link Document} that represents the same data as the handled JSON document
+ * using the following rules:
+ * 
+ * <ul>
+ * <li>A JSON array is represented as a tag with name {@code array}.</li>
+ * <li>A JSON object is represented as a tag with name {@code object}.</li>
+ * <li>A JSON object entry is represented as a tag with name {@code entry}. The
+ * name of the JSON object entry is represented as an attribute with name
+ * {@code name}.</li>
+ * <li>A JSON null is represented as a tag with name {@code null}.</li>
+ * <li>A JSON boolean is represented as a tag with name {@code boolean}. The
+ * value of the JSON boolean represented as an attribute with name {@code value}
+ * .</li>
+ * <li>A JSON number is represented as a tag with name {@code number}. The value
+ * of the JSON number represented as an attribute with name {@code value}.</li>
+ * <li>A JSON string is represented as a tag with name {@code string}. The value
+ * of the JSON string represented as an attribute with name {@code value} .</li>
+ * </ul>
+ * 
+ * <p>
+ * <b>Example:</b>
+ * 
+ * <p>
+ * Input JSON document:
+ * 
+ * <pre>
+ * {
+ *     "nullValue": null,
+ *     "booleanValue": true,
+ *     "longValue": -10000,
+ *     "doubleValue": 42.23,
+ *     "stringValue": "foobar",
+ *     "arrayValue": [],
+ *     "objectValue": {}
+ * }
+ * </pre>
+ * 
+ * Output XML document:
+ * 
+ * <pre>
+ * {@code
+ * <?xml version="1.0" encoding="UTF-8"?>
+ * <object>
+ *    <entry name="nullValue">
+ *       <null />
+ *    </entry>
+ *    <entry name="booleanValue">
+ *       <boolean value="true" />
+ *    </entry>
+ *    <entry name="longValue">
+ *       <number value="-10000"/>
+ *    </entry>
+ *    <entry name="doubleValue">
+ *       <number value="42.23" />
+ *    </entry>
+ *    <entry name="stringValue">
+ *       <string value="foobar">
+ *    </entry>
+ *    <entry name="arrayValue">
+ *       <array />
+ *    </entry>
+ *    <entry name="objectValue">
+ *       <object />
+ *    </entry>
+ * </object>
+ * }
+ * </pre>
+ * 
+ *
+ * @author Torsten Krause (tk at markenwerk dot net)
+ * @since 1.0.0
+ */
+public final class XmlDocumentJsonHandler extends IdleJsonHandler<Document> {
+
+	private final Document document;
+
+	private Node node;
+
+	/**
+	 * Creates a new {@link XmlDocumentJsonHandler} using a @link
+	 * {@link DocumentBuilderFactory#newDocumentBuilder() default}
+	 * {@link DocumentBuilder} created by a
+	 * {@link DocumentBuilderFactory#newInstance() default}
+	 * {@link DocumentBuilderFactory}.
+	 */
+	public XmlDocumentJsonHandler() {
+		this(createDocumentBuilder());
+	}
+
+	private static DocumentBuilder createDocumentBuilder() {
+		try {
+			return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new RuntimeException("The default DocumentBuilder faild to create a XML Document", e);
+		}
+	}
+
+	/**
+	 * Create a new {@link XmlDocumentJsonHandler} using the given
+	 * {@link DocumentBuilder} to create a new XML {@link Document}.
+	 * 
+	 * @param documentBuilder
+	 *            The {@link DocumentBuilder} to be used.
+	 * @throws IllegalArgumentException
+	 *             If the given {@link DocumentBuilder} is {@literal null}.
+	 */
+	public XmlDocumentJsonHandler(DocumentBuilder documentBuilder) throws IllegalArgumentException {
+		if (null == documentBuilder) {
+			throw new IllegalArgumentException("documentBuilder is null");
+		}
+		document = documentBuilder.newDocument();
+		node = document;
+	}
+
+	@Override
+	public void onDocumentBegin() throws JsonHandlingException {
+	}
+
+	@Override
+	public void onDocumentEnd() throws JsonHandlingException {
+	}
+
+	@Override
+	public void onArrayBegin() throws JsonHandlingException {
+		Element element = document.createElement("array");
+		this.node.appendChild(element);
+		this.node = element;
+	}
+
+	@Override
+	public void onArrayEnd() throws JsonHandlingException {
+		node = node.getParentNode();
+		if (node.getNodeName().equals("entry")) {
+			node = node.getParentNode();
+		}
+	}
+
+	@Override
+	public void onObjectBegin() throws JsonHandlingException {
+		Element element = document.createElement("object");
+		this.node.appendChild(element);
+		this.node = element;
+	}
+
+	@Override
+	public void onObjectEnd() throws JsonHandlingException {
+		node = node.getParentNode();
+		if (node.getNodeName().equals("entry")) {
+			node = node.getParentNode();
+		}
+	}
+
+	@Override
+	public void onName(String name) throws JsonHandlingException {
+		Element element = document.createElement("entry");
+		element.setAttribute("name", name);
+		this.node.appendChild(element);
+		this.node = element;
+	}
+
+	@Override
+	public void onNext() throws JsonHandlingException {
+	}
+
+	@Override
+	public void onNull() throws JsonHandlingException {
+		Element element = document.createElement("null");
+		appendChild(element);
+	}
+
+	@Override
+	public void onBoolean(boolean value) throws JsonHandlingException {
+		Element element = document.createElement("boolean");
+		element.setAttribute("value", value ? "true" : "false");
+		appendChild(element);
+	}
+
+	@Override
+	public void onLong(long value) throws JsonHandlingException {
+		Element element = document.createElement("number");
+		element.setAttribute("value", Long.toString(value));
+		appendChild(element);
+	}
+
+	@Override
+	public void onDouble(double value) throws InvalidJsonValueException, JsonHandlingException {
+		checkDoubleValue(value);
+		Element element = document.createElement("number");
+		element.setAttribute("value", Double.toString(value));
+		appendChild(element);
+	}
+
+	@Override
+	public void onString(String value) throws InvalidJsonValueException, JsonHandlingException {
+		checkStringValue(value);
+		Element element = document.createElement("string");
+		element.setAttribute("value", value);
+		appendChild(element);
+	}
+
+	private void appendChild(Element element) {
+		node.appendChild(element);
+		if (node.getNodeName().equals("entry")) {
+			node = node.getParentNode();
+		}
+	}
+
+	@Override
+	public Document getResult() throws JsonHandlingException {
+		return document;
+	}
+
+}
---
+++ src/test/java/net/markenwerk/utils/json/common/handler/xml/XmlDocumentJsonHandlerTests.java
@@ -0,0 +1,329 @@
+package net.markenwerk.utils.json.common.handler.xml;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.builder.Input;
+import org.xmlunit.diff.Diff;
+
+import net.markenwerk.utils.json.common.InvalidJsonValueException;
+import net.markenwerk.utils.json.handler.JsonHandler;
+
+@SuppressWarnings("javadoc")
+public class XmlDocumentJsonHandlerTests {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void create_nullDocumentBuilder() {
+
+		new XmlDocumentJsonHandler(null);
+
+	}
+
+	@Test
+	public void onNull() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onNull();
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<null />", document);
+
+	}
+
+	@Test
+	public void onBoolean_true() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onBoolean(true);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<boolean value=\"true\" />", document);
+
+	}
+
+	@Test
+	public void onBoolean_false() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onBoolean(false);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<boolean value=\"false\" />", document);
+
+	}
+
+	@Test
+	public void onLong_zero() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onLong(0);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<number value=\"" + Long.toString(0) + "\" />", document);
+
+	}
+
+	@Test
+	public void onLong_positive() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onLong(Long.MAX_VALUE);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<number value=\"" + Long.toString(Long.MAX_VALUE) + "\" />", document);
+
+	}
+
+	@Test
+	public void onLong_negative() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onLong(Long.MIN_VALUE);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<number value=\"" + Long.toString(Long.MIN_VALUE) + "\" />", document);
+
+	}
+
+	@Test(expected = InvalidJsonValueException.class)
+	public void onDouble_notANumber() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onDouble(Double.NaN);
+		handler.onDocumentEnd();
+
+	}
+
+	@Test(expected = InvalidJsonValueException.class)
+	public void onDouble_infinite() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onDouble(Double.POSITIVE_INFINITY);
+		handler.onDocumentEnd();
+
+	}
+
+	@Test
+	public void onDouble_zero() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onDouble(0);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<number value=\"" + Double.toString(0) + "\" />", document);
+
+	}
+
+	@Test
+	public void onDouble_positive() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onDouble(Double.MAX_VALUE);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<number value=\"" + Double.toString(Double.MAX_VALUE) + "\" />", document);
+
+	}
+
+	@Test
+	public void onDouble_negative() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onDouble(Double.MIN_VALUE);
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<number value=\"" + Double.toString(Double.MIN_VALUE) + "\" />", document);
+
+	}
+
+	@Test(expected = InvalidJsonValueException.class)
+	public void onString_null() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onString(null);
+		handler.onDocumentEnd();
+
+	}
+
+	@Test
+	public void onString_empty() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onString("");
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<string value=\"\"/>", document);
+
+	}
+
+	@Test
+	public void onString_nonEmpty() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onString("foobar");
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<string value=\"foobar\"/>", document);
+
+	}
+
+	@Test
+	public void onArray_empty() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onArrayBegin();
+		handler.onArrayEnd();
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<array />", document);
+
+	}
+
+	@Test
+	public void onArray_nonEmpty() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onArrayBegin();
+		handler.onNull();
+		handler.onArrayEnd();
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<array><null /></array>", document);
+
+	}
+
+	@Test
+	public void onObject_empty() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onObjectBegin();
+		handler.onObjectEnd();
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<object />", document);
+
+	}
+
+	@Test
+	public void onObject_nonEmpty() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onObjectBegin();
+		handler.onName("n");
+		handler.onNull();
+		handler.onObjectEnd();
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals("<object><entry name=\"n\"><null /></entry></object>", document);
+
+	}
+
+	@Test
+	public void onDocument_complex() {
+
+		JsonHandler<Document> handler = new XmlDocumentJsonHandler();
+
+		handler.onDocumentBegin();
+		handler.onObjectBegin();
+		handler.onName("n");
+		handler.onNull();
+		handler.onNext();
+		handler.onName("b");
+		handler.onBoolean(true);
+		handler.onNext();
+		handler.onName("l");
+		handler.onLong(-42);
+		handler.onNext();
+		handler.onName("d");
+		handler.onDouble(-23.42);
+		handler.onNext();
+		handler.onName("a");
+		handler.onArrayBegin();
+		handler.onString("foo");
+		handler.onNext();
+		handler.onString("bar");
+		handler.onArrayEnd();
+		handler.onObjectEnd();
+		handler.onDocumentEnd();
+
+		Document document = handler.getResult();
+
+		assertEquals(
+				"<object><entry name=\"n\"><null /></entry><entry name=\"b\"><boolean value=\"true\" /></entry><entry name=\"l\"><number value=\"-42\" /></entry><entry name=\"d\"><number value=\"-23.42\" /></entry><entry name=\"a\"><array><string value=\"foo\" /><string value=\"bar\" /></array></entry></object>",
+				document);
+
+	}
+
+	private void assertEquals(String expected, Document actual) {
+		Diff diff = DiffBuilder.compare(Input.fromString(expected)).withTest(Input.fromDocument(actual)).build();
+		Assert.assertFalse(diff.toString(), diff.hasDifferences());
+	}
+
+}
